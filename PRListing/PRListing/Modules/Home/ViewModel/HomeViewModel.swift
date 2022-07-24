47db998789cb6ad4b4e986cfc775a10ec76e3b96
Tania Jasam <tania.j@pepperfry.com>
2022-07-24 17:07:24 +0530

    Added viewModel

--- PRListing/PRListing/Modules/Home/ViewModel/HomeViewModel.swift
+++ PRListing/PRListing/Modules/Home/ViewModel/HomeViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Combine
+
+class HomeViewModel: ObservableObject {
+    
+    lazy var subscriptions: Set<AnyCancellable> = Set<AnyCancellable>()
+    var networkManager: NetworkRequestable?
+    @Published var listingData: [PRListingModel] = []
+    
+    init(networkManager: NetworkRequestable) {
+        self.networkManager = networkManager
+    }
+    
+    func fetchPullRequests() {
+        networkManager?.dataTask(with: HomeAPIConfiguration(), type: PRListingModel.self)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    
+                default:
+                    break
+                }
+            }, receiveValue: { [weak self] response in
+                self?.listingData = response
+            })
+            .store(in: &subscriptions)
+    }
+}
--- PRListing/PRListing/Modules/Home/Views/HomeViewController.swift
+++ PRListing/PRListing/Modules/Home/Views/HomeViewController.swift
@@ -6,11 +6,26 @@
 //
 
 import UIKit
+import Combine
 
 class HomeViewController: UIViewController {
+    
+    private var viewModel: HomeViewModel = HomeViewModel(networkManager: NetworkManager.sharedInstance)
+    var subscriptions: Set<AnyCancellable> = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.bindViewModel()
+        self.viewModel.fetchPullRequests()
+    }
+    
+    func bindViewModel() {
+        viewModel.$listingData
+            .receive(on: RunLoop.main)
+            .sink { listingData in
+                print(listingData.count)
+            }
+            .store(in: &subscriptions)
     }
 
 }
